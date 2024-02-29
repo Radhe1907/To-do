302101ffdeb26b36e931b5291d684e64862eca48
Pratik Pandey <150671823+Radhe1907@users.noreply.github.com>
2024-02-29 18:49:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const search = document.getElementById("search");
+const listcontainer = document.getElementById("list-container");
+const addbutton = document.getElementsByClassName("but");
+
+function addclass() {
+  if (search.value === "") {
+    alert("Please add your to-do's");
+    document.querySelector(".clear").style.display = "none";
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = search.value;
+    listcontainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    document.querySelector(".clear").style.display = "block";
+  }
+  search.value = "";
+  saveData();
+}
+listcontainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+function clearall() {
+  listcontainer.innerHTML = "";
+  document.querySelector(".clear").style.display = "none";
+  saveData();
+}
+function saveData() {
+  localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showData() {
+  listcontainer.innerHTML = localStorage.getItem("data");
+  listcontainer.innerHTML === ""
+    ? (document.querySelector(".clear").style.display = "none")
+    : (document.querySelector(".clear").style.display = "block");
+}
+showData();
